bc07ca1d2e44cea69e86917921ed43a9bc88d566
Sai Rishit.S <56475400+Rishit123344@users.noreply.github.com>
2021-05-25 17:55:14 +0530

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,25 @@
+import csv
+dataset1 = []
+dataset2 = []
+with open ('dataset1.csv')as f:
+    csvreader = csv.reader(f)
+    for row in csvreader:
+        dataset1.append(row)
+headers1 = dataset1[0]
+planetdata1 = dataset1[1:]
+with open ('dataset_2_sorted.csv')as f:
+    csvreader = csv.reader(f)
+    for row in csvreader:
+        dataset2.append(row)
+headers2 = dataset2[0] 
+planetdata2 = dataset2[1:]
+headers =  headers1+headers2
+planetdata = []
+for index,datarow in enumerate(planetdata1):
+    planetdata.append(planetdata1[index]+planetdata2[index])
+with open('final1.csv','a+')as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(headers)
+    csvwriter.writerows(planetdata)
+
+      
